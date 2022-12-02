32b17cfa7e78feaaa6db32e7fc263d357dd76027
Noah Pope <npope@tutanota.com>
2022-12-02 08:03:58 -0500

    Conforming to MVC and created a CollectionViewTableViewCelll class

---
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Noah Pope on 12/2/22.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
