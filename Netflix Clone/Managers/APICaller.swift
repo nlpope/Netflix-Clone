21762b7f63ee15970a41ef72a8b413a475058d63
Noah Pope <npope@tutanota.com>
2022-12-13 07:57:31 -0500

    got api key from TMDB. Researching completion handlers

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,22 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Noah Pope on 12/13/22.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let API_KEY = "a33493c4a470b7abe6133ac90dfaed66"
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        
+        
+    }
+}
