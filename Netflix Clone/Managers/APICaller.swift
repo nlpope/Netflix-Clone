4fdcca615242def31a3d2b8da50eff68054c4fb7
Noah Pope <npope@tutanota.com>
2022-12-15 07:58:40 -0500

    fleshing out the APICaller

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -10,6 +10,7 @@ import Foundation
 
 struct Constants {
     static let API_KEY = "a33493c4a470b7abe6133ac90dfaed66"
+    static let baseURL = "https://api.themoviedb.org"
 }
 
 class APICaller {
@@ -17,9 +18,21 @@ class APICaller {
     
     func getTrendingMovies(completion: @escaping (String) -> Void) {
         
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
         
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {return}
+            
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
     }
 }
 
-//doing research on closures -> complHandlers
-//finished closure readings/practice -> moving on to complHanlders
+//doing research on completion handlers (escaping & non-esc.)
