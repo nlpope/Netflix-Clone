05cbe0c9c43105627999490d5cc72bdc49d8a4f1
Noah Pope <npope@tutanota.com>
2022-12-21 12:59:32 -0500

    defined initializers for TitleCollectionViewCell class

--- Netflix Clone/Views/TitleCollectionViewCell.swift
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -9,4 +9,15 @@ import UIKit
 
 class TitleCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "TitleCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
 }
