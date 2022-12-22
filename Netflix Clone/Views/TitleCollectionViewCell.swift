5f6db20a2988710ee622fffbc99b6de899c706e0
Noah Pope <npope@tutanota.com>
2022-12-22 07:51:09 -0500

    set up collectionViewCell to eventually fill green squares

--- Netflix Clone/Views/TitleCollectionViewCell.swift
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Noah Pope on 12/21/22.
 //
+//  Responsible for handling everything inside collection view instead of default cell we get
 
 import UIKit
 
@@ -11,13 +12,26 @@ class TitleCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "TitleCollectionViewCell"
     
+    //anonymous closure
+    private let posterImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        contentView.addSubview(posterImageView)
     }
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
     }
     
 }
