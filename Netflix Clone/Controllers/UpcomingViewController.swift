4c361213e4ca889c228823092330ffcb3fc0bd9f
Noah Pope <npope@tutanota.com>
2022-12-28 07:42:33 -0500

    populated Upcoming view controller w list of upcoming movies; time to make cells

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
     
+    private var titles: [Title] = [Title]()
+    
     private let upcomingTable: UITableView = {
        //we're only using the URL here so use closure based initialization
         let table = UITableView()
@@ -26,11 +28,28 @@ class UpcomingViewController: UIViewController {
         view.addSubview(upcomingTable)
         upcomingTable.delegate = self
         upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
 
@@ -39,12 +58,12 @@ class UpcomingViewController: UIViewController {
 
 extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown"
         return cell
     }
     
