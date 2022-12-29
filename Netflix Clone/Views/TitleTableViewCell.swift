b5d23919933ac3e62e3f338da128a07ef76a534d
Noah Pope <npope@tutanota.com>
2022-12-29 07:45:24 -0500

    setting up title table view cell for "upcoming" view controller

--- Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -11,9 +11,54 @@ class TitleTableViewCell: UITableViewCell {
 
   static let identifier = "TitleTableViewCell"
     
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    //creating ui image view to hold the poster
+    private let titlePosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     //initializers are not inherited for subclasses by default; Parent (UITableViewCell) already has this initializer
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: "")
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        
+        applyConstraints()
     }
-
+    
+    
+    private func applyConstraints() {
+        let titlesPosterUIImageViewConstraints = [
+            titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(titlesPosterUIImageViewConstraints)
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
 }
