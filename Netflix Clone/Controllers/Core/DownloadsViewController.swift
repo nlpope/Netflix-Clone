0a622f5f1e83b10dee7fbb6f2792ff4ab71f7449
Noah Pope <npope@tutanota.com>
2023-01-31 07:57:39 -0500

    Populating DownloadsViewController

--- Netflix Clone/Controllers/Core/DownloadsViewController.swift
+++ Netflix Clone/Controllers/Core/DownloadsViewController.swift
@@ -8,15 +8,47 @@
 import UIKit
 
 class DownloadsViewController: UIViewController {
+    
+    private let titles: [Title] = [Title]()
+    
+    //type that retains completion handlers
+    private let downloadedTable: UITableView = {
+       //we're only using the URL here so use closure based initialization
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
+        title = "Downloads"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        downloadedTable.delegate = self
+        downloadedTable.dataSource = self
 
     }
+}
+
+extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
     
-
-   
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: (title.original_title ?? title.original_name) ?? "Unknown", posterURL: title.poster_path ?? ""))
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
